5bf7511999bbe5011fc3a5d1c89ef2d898d80221
soljjang777 <vivace8520vi@naver.com>
2024-08-02 15:26:16 +0900

    mod : 초기 프로젝트 구조 생성

---
+++ src/main/java/com/b1aboa/wedug/WedugApplication.java
@@ -0,0 +1,13 @@
+package com.b1aboa.wedug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WedugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b1aboa/wedug/WedugApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1aboa.wedug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
