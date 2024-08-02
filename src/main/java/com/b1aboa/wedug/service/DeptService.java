313a985d93731ae4d66515302c7557541e3665a6
hongminyeong <minyung1240@daum.net>
2024-08-02 20:29:11 +0900

    feat: db 연동 test log찍기

--- src/main/java/com/b1aboa/wedug/service/DeptService.java
+++ src/main/java/com/b1aboa/wedug/service/DeptService.java
@@ -5,10 +5,14 @@ import com.b1aboa.wedug.repository.DeptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import jakarta.annotation.PostConstruct;
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class DeptService {
+    // Logger 인스턴스 생성
+    private static final Logger logger = Logger.getLogger(DeptService.class.getName());
 
     @Autowired
     private DeptRepository deptRepository;
@@ -19,5 +23,13 @@ public class DeptService {
 
     public Dept saveDept(Dept dept) {
         return deptRepository.save(dept);
+    }
+
+    @PostConstruct
+    public void logAllDepts() {
+        // 데이터베이스에서 모든 부서 데이터 조회
+        List<Dept> depts = deptRepository.findAll();
+        // 각 부서 데이터를 로그에 출력
+        depts.forEach(dept -> logger.info(dept.toString()));
     }
 }
