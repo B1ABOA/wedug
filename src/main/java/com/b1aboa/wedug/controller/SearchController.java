47a9708b55d95fbf475d0a3833023a7441fa6cdf
hongminyeong <minyung1240@daum.net>
2024-08-14 01:48:04 +0900

    feat: searchList view 단으로 가져오기
    @ResponseBody 로 json 직렬화

--- src/main/java/com/b1aboa/wedug/controller/SearchController.java
+++ src/main/java/com/b1aboa/wedug/controller/SearchController.java
@@ -1,13 +1,13 @@
 package com.b1aboa.wedug.controller;
 
 import com.b1aboa.wedug.dto.PlaceDTO;
-import com.b1aboa.wedug.entity.Place;
 import com.b1aboa.wedug.service.SearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -29,10 +29,20 @@ public class SearchController {
 
 //     /api/places/search?media-type={mediaType}
     @GetMapping("/api/places/search")
-    public String searchPlaceMedia(@RequestParam("media-type") String mediaType, Model model) {
+    @ResponseBody
+    public List<PlaceDTO> searchPlaceMedia(@RequestParam("media-type") String mediaType,
+                               @RequestParam(value = "keyword", required = false) String keyword,
+                                   @RequestParam(value = "place", required = false) String place) {
         List<PlaceDTO> searchList = searchService.searchPlacesByMediaType(mediaType);
-        model.addAttribute("searchList", searchList);
-        return "search/map-search";
+
+
+        return searchList;
     }
+//    @GetMapping("/api/places/search")
+//    public String searchPlaceMedia(@RequestParam("media-type") String mediaType, Model model) {
+//        List<PlaceDTO> searchList = searchService.searchPlacesByMediaType(mediaType);
+//        model.addAttribute("searchList", searchList);
+//        return "search/map-search";
+//    }
 
 }
