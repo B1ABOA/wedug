8f10e928194caaacd87ea03f9e969715c058f8a1
dndqlszzz <mandarin1999@naver.com>
2024-08-15 18:57:00 +0900

    feat: jwt토큰 발급기능 구현 - 로그인 성공 시 Client의 로컬스토리지에 jwt 토큰이 저장됩니다. - 로그인 실패 시 화면 전환은 아직 미구현.

--- src/main/java/com/b1aboa/wedug/jwt/LoginFilter.java
+++ src/main/java/com/b1aboa/wedug/jwt/LoginFilter.java
@@ -46,7 +46,7 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
 
     // 로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됩니다.)
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication){
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException {
         log.info("SUCCESS_AUTH START");
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
         String userId = customUserDetails.getUsername();
@@ -56,7 +56,12 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
         //RFC 7235 정의에 따른 인증 헤더 형태 -> Authorization: Bearer 인증토큰 string
         // ** 주의 Bearer 뒤에 공백 하나 있어야 함.
         // jwt 토큰 헤더에 추가하여 반환
-        response.addHeader("Authorization", "Bearer " + token);
+        // response.addHeader("Authorization", "Bearer " + token);
+
+        response.setContentType("application/json");
+        response.getWriter().write("{\"token\":\"" + token + "\"}");
+        response.getWriter().flush();
+
         log.info("SUCCESS_AUTH FINISH");
     }
 
